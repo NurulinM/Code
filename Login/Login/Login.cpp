cf193e53d1baaa71b3af1fcae760726f5002ece7
Konstantin <91050739+NurulinM@users.noreply.github.com>
2022-05-05 12:39:52 +0300

    Whith bugs

--- Login/Login/Login.cpp
+++ Login/Login/Login.cpp
@@ -7,44 +7,79 @@ string pass[1];
 string chUser[1];
 string chPass[1];
 //-----------------------------------------------
-//обявление функций на шапке програмы
+//обявление функций на шапке программы
 void check();
 //-----------------------------------------------
-string SingUp;
+string Login;
+bool i;
 
 int main()
 {
-    cout << setw(40)<< "Login" <<endl;
+    cout << setw(40)<< "Sing up" <<endl;
     cout << "Name of user ";
     cin >> user[0];
     cout << "Password ";
     cin >> pass[0];
     cout << "You registred"<<endl;
-    cout << "Sing up (yes/no) "<<endl;
-    cin >> SingUp;
-    if (SingUp == "yes")
+    cout << "Login (yes/no) "<<endl;
+    cin >> Login;
+    
+        if (Login == "yes")
         {
             check();
         }
-    if (SingUp == "no")
+        if (Login == "no")
         {
-        cout <<  "OK";
+            cout << "OK";
         }
+        else
+        {
+            while (Login != "yes", Login != "no")
+            {
+                cout << "Login (yes/no) " << endl;
+                cin >> Login;
+
+                if (Login == "yes")
+                {
+                    check();
+                }
+                if (Login == "no")
+                {
+                    cout << "OK";
+                }
+            }
+        }
+    
     return 0;
 }
 //функция проверки
 void check()
 {
-    do
+    for (i = false; i == false ;)
     {
-        cout << setw(45) << "Sing Up" << endl;
-        cout << "User ";
-        cin >> chUser[0];
-        cout << "Password ";
-        cin >> chPass[0];
-        
-    } while (user[0] != chUser[0] ,  pass[0] != chPass[0]);
-        cout << "You Sing Up";
+
+
+        do
+        {
+            cout << setw(45) << "Login" << endl;
+            cout << "User ";
+            cin >> chUser[0];
+            cout << "Password ";
+            cin >> chPass[0];
+
+        } while (user[0] != chUser[0], pass[0] != chPass[0]);
+
+
+        if (user[0] == chUser[0], pass[0] == chPass[0])
+        {
+            i = true;
+        }
+        else
+        {
+            i = false;
+        }
+    }
+        cout << "You Login";
 }
 
 
